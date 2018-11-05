4b550bb40bd7690ba1ba0d1cf7c2fe827aca7031
Unknown <dollapat.966@hotmail.com>
2018-11-05 18:47:01 +0700

    Edit Login & Subject page but controller not complete

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,5 +1,6 @@
 package sample;
 
+import com.sun.deploy.util.SessionState;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,32 +12,48 @@ import org.hibernate.cfg.Configuration;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import java.io.File;
+import java.net.URL;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 //        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-//        primaryStage.setTitle("I-ClassRoom");
-//        primaryStage.setScene(new Scene(root, 1920, 1080));
-//        primaryStage.show();
+        URL url = new File("src/main/java/sample/login.fxml").toURL();
+        Parent root = FXMLLoader.load(url);
+        primaryStage.setTitle("I-ClassRoom");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+//        Scanner reader = new Scanner(System.in);
+//        int n = reader.nextInt();
+//        reader.close();
+//        if(n == 1){
+//            url = new File("src/main/java/sample/subject.fxml").toURL();
+//            root = FXMLLoader.load(url);
+//            primaryStage.setScene(new Scene(root));
+//        }
+//        System.out.printf("%d", n);
+        //LoginController login = new LoginController(primaryStage);
     }
 
 
     public static void main(String[] args) {
-//        launch(args);
-        SessionFactory factory;
-        Session session;
-
-        factory = new Configuration().configure().buildSessionFactory();
-        session = factory.openSession();
-        session.beginTransaction();
-        User user = new User("Hello Chai");
-        session.save(user);
-        session.getTransaction().commit();
-        factory.close();
-        System.out.println("Started");
+        launch(args);
+//        SessionFactory factory;
+//        Session session;
+//
+//        factory = new Configuration().configure().buildSessionFactory();
+//        session = factory.openSession();
+//        session.beginTransaction();
+//        User user = new User("Hello Chai");
+//        session.save(user);
+//        session.getTransaction().commit();
+//        factory.close();
+//        System.out.println("Started");
 
 
     }
