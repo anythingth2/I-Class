c13b443d16ec93e38e36e36ec8dd90cb5a45f1a1
MeowKawaii <pingpong_jane@hotmail.com>
2018-11-11 22:07:43 +0700

    change file name

--- src/main/java/UI/Course/CourseController.java
+++ src/main/java/UI/Course/CourseController.java
@@ -1,4 +1,37 @@
-package UI.Course;
+package UI.Teacher;
 
-public class CourseController {
+import Model.Material;
+import Model.TeachingClass;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+
+import java.util.Date;
+
+public class TeacherHomeController {
+    @FXML
+    VBox classVBox;
+    @FXML
+    ScrollPane scrollPane;
+    @FXML
+    Button button;
+
+    public TeacherHomeController() {
+        super();
+    }
+
+    @FXML
+    void onAddClick() {
+        System.out.println("clicked");
+        TeachingClass teachingClass = new TeachingClass();
+        teachingClass.setDate(new Date(System.currentTimeMillis()));
+        ClassItemPane child = new ClassItemPane(teachingClass);
+
+
+        this.classVBox.getChildren().add(child);
+
+    }
 }
