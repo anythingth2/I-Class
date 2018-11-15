4c0afa7b9c91b34933d305125aec7627b02a6c50
MeowKawaii <pingpong_jane@hotmail.com>
2018-11-15 08:35:15 +0700

    edit dialog and and text alert

--- src/main/java/UI/Dialog/AnnouncementDialog/announcementDialogController.java
+++ src/main/java/UI/Dialog/AnnouncementDialog/announcementDialogController.java
@@ -1,4 +1,18 @@
 package UI.Dialog.AnnouncementDialog;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class announcementDialogController {
+
+    @FXML
+    private Label cancelButton;
+
+    @FXML
+    private Label editButton;
+
+    @FXML
+    private TextField dataField;
+
 }
--- src/main/java/UI/Dialog/comfirmDialog/comfirmDialogController.java
+++ src/main/java/UI/Dialog/comfirmDialog/comfirmDialogController.java
@@ -1,4 +1,15 @@
 package UI.Dialog.comfirmDialog;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class comfirmDialogController {
+
+    @FXML
+    private Label noButton;
+
+    @FXML
+    private Label yesButton;
+
 }
+
