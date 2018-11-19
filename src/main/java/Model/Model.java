427e15099c1c08a92b17158d486f2f8170f8829b
Sarunyu Chankong <sarunyu.you@gmail.com>
2018-11-19 23:58:40 +0700

    Remove unused package

--- src/main/java/Model/Model.java
+++ src/main/java/Model/Model.java
@@ -1,14 +1,11 @@
 package Model;
 
 import Main.HibernateUtil;
-import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import org.hibernate.criterion.Restrictions;
 
 public class Model {
     // Begin generally ORM methods
-
     public static void save(Model obj){
         try {
             Session session = HibernateUtil.getSessionFactory().openSession();
